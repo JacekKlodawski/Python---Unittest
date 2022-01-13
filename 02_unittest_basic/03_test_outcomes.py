c19b6b371c2c5700bf1bbe14fe9e1a3464cae29f
Jacek <jacek.klodawski@gmail.com>
2022-01-13 22:44:37 +0100

    added test outcomes

--- 02_unittest_basic/03_test_outcomes.py
+++ 02_unittest_basic/03_test_outcomes.py
@@ -13,20 +13,22 @@ def area(width, height):
 
     return width * height
 
+
 class TestArea(unittest.TestCase):
 
-    def test_area(self):
+    def test_area_1(self):
         self.assertEqual(area(4, 5), 20)
 
-    def test_area_incorrect_type_should_raise_error(self):
-        self.assertRaises(TypeError, area, "4", 5)
-        self.assertRaises(TypeError, area, 4, "5")
+    def test_area_2(self):
+        self.assertEqual(area(4, 5), 21)
 
-    def test_area_negative_value_should_raise_error(self):
-        self.assertRaises(ValueError, area, -4, 5)
-        self.assertRaises(ValueError, area, 4, -5)
+    def test_area_3(self):
+        raise AssertionError('Error Message')
+
+    def test_area_4(self):
+        raise TypeError('Error Message')
+
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
 
-    
