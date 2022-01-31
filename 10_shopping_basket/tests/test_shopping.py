1fccad38f7411fc94895e21e0a89dff8ce88d2bd
Jacek <jacek.klodawski@gmail.com>
2022-01-31 19:16:33 +0100

    added shopping basket tests

---
+++ 10_shopping_basket/tests/test_shopping.py
@@ -0,0 +1,22 @@
+import unittest
+from shopping.basket import ShoppingBasket
+
+class TestShoppingBasketWithNoProduct(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        print('\n[INFO] Setting up basket without any products...')
+        cls.basket = ShoppingBasket()
+
+    def test_size_of_basket_should_be_empty(self):
+        pass
+
+    def test_getting_product_out_or_range_should_raise_error(self):
+        pass
+
+    def test_total_amount_should_be_zero(self):
+        pass
+
+class TestShoppingBasketWithOneProduct():
+
+
