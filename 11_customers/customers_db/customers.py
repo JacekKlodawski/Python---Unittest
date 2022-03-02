795ace39fbc62998092e0bbb9e29a91347128019
Jacek <jacek.klodawski@gmail.com>
2022-03-02 20:53:50 +0100

    added CustomersDB

---
+++ 11_customers/customers_db/customers.py
@@ -0,0 +1,18 @@
+class CustomersDB:
+
+    def __init__(self, connection):
+        self.connection = connection
+
+    def add_customer(
+        self,
+        first_name,
+        second_name,
+        email,
+        phone,
+        country
+    ):
+        cursor = self.connection.cursor()
+        sql = """
+        INSERT INTO customers
+        VALUES
+        """
